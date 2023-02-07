a812dadddb86e29e1833212c1a5d2267a10a4cf7
Sean M. Patrick <smp429@ilab2.cs.rutgers.edu>
2023-02-07 17:52:54 -0500

    Making helper functions

--- mymalloc.c
+++ mymalloc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "mymalloc.h"
 
 #define MEMSIZE 4096
@@ -8,9 +9,21 @@
 
 static char memory[MEMSIZE];
 
-void insertMetaData(void *start, short blockSize){
+void insertMetaData(void *start, short chunkSize){
     short *size = (short *) start;
-    *size = blockSize;
+    *size = chunkSize;
+}
+
+short getChunkSize(void *start){
+    short *chunkSize = (short *) start;
+    return *chunkSize;
+}
+
+bool isChunkFree(void *start){
+    if(getChunkSize(start) < 0){
+        return true;
+    }
+    return false;
 }
 
 void initializeMemory(){
@@ -20,9 +33,9 @@ void initializeMemory(){
         *start = KEY; //using a key to determine if the memory array has been initialized before using the memory
         char *metaDataStart = (char *) memory + sizeof(unsigned short);
         char *end = (char *) memory + MEMSIZE;
-        short blockSize = -(end - metaDataStart - sizeof(short));
-        //printf("%d", blockSize);
-        insertMetaData((void *)metaDataStart, blockSize);
+        short chunkSize = -(end - metaDataStart - sizeof(short));
+        //printf("%d", chunkSize);
+        insertMetaData((void *)metaDataStart, chunkSize);
     }
 }
 
