f049dd2c18e2defd19f9746e0f7923bd3f53ba46
Sean M. Patrick <smp429@ilab2.cs.rutgers.edu>
2023-02-08 16:30:57 -0500

    Added more helper functions and adjusted memory initialization and metadata features

--- README.txt
+++ README.txt
@@ -1,2 +1,10 @@
 Sean Patrick & Fulton Wilcox
 smp429 & frw14
+
+Memory is uninitialized at first, with a single '_' character placed at the beginning.
+This character is used as a flag to determine if memory needs to be initialzed.
+Upon the first call to mymalloc, we check if the '_' character exists, and if it does then we initialize all bytes to 0
+Then we allocate a short integer to represent the size of the subsequent memory chunk (metadata).
+After the first mymalloc call, memory will consist of 2 bytes which represent 4094, and then the remaining free 4094 bytes.
+For every subsequent memory allocation, 2 bytes will be reserved to store the metadata.
+To determine if a chunk of memory is in use or not, we will represent the 2 byte metadata as positive (free) or negative (allocated).
--- mymalloc.c
+++ mymalloc.c
@@ -5,9 +5,9 @@
 #include "mymalloc.h"
 
 #define MEMSIZE 4096
-#define INITFLAG 7421
+#define INITFLAG '_'
 
-static char memory[MEMSIZE];
+static char memory[MEMSIZE] = {INITFLAG};
 
 void insertMetaData(void *memPtr, short chunkSize){
     short *metaData = (short *) memPtr;
@@ -20,23 +20,62 @@ short getChunkSize(void *memPtr){
 }
 
 bool isChunkFree(void *memPtr){
-    if(getChunkSize(memPtr) < 0){
+    if(getChunkSize(memPtr) > 0){
         return true;
     }
     return false;
 }
 
 void initializeMemory(){
-    short *memPtr = (short *) memory;
-    if(*memPtr != INITFLAG){
-        memset((void *)memory, 0, MEMSIZE);
-        *memPtr = INITFLAG; //using a flag to determine if the memory array has been initialized before using the memory
-        char *metaDataPtr = (char *) memory + sizeof(short);
-        char *memEndPtr = (char *) memory + MEMSIZE;
+    char *memPtr = memory;
+    if(*memPtr == INITFLAG){ //using a flag to determine if the memory array has been initialized before using the memory
+        memset(memory, 0, MEMSIZE);
+        char *metaDataPtr = memory;
+        char *memEndPtr = memory + MEMSIZE;
         short chunkSize = (memEndPtr - metaDataPtr - sizeof(short));
         //printf("%d", chunkSize);
-        insertMetaData((void *)metaDataPtr, chunkSize);
+        insertMetaData(metaDataPtr, chunkSize);
     }
+}
+
+void *firstMetaData(){
+    char *memPtr = memory;
+    return memPtr; //returns address of first memory chunk's metadata
+}
+
+void *nextMetaData(void *memPtr){ //returns address of next chunk's metadata given a starting chunk, NULL if there are no more chunks
+    char *nextChunkMetaData = memPtr;
+    char *memEnd = memory + MEMSIZE;
+    short chunkSize = getChunkSize(memPtr);
+    if(chunkSize < 0){
+        chunkSize *= -1;
+    }
+    nextChunkMetaData += chunkSize + sizeof(short);
+    if(memEnd > nextChunkMetaData){
+        return nextChunkMetaData;
+    }
+    return NULL;
+}
+
+bool validPointer(void *memPtr){ //checks if a given pointer is within the bounds of the memory array
+    char *memStart = firstMetaData();
+    char *memEnd = memory + MEMSIZE;
+    char *ptr = memPtr;
+    if(ptr >= memStart && ptr < memEnd){
+        return true;
+    }
+    return false;
+}
+
+bool completePointer(void *memPtr){ //checks if a given pointer is complete(If it correctly points to the beginning of the memory chunk (its metadata) and not somewhere in between)
+    void *metaData = firstMetaData();
+    while(metaData != NULL){
+        if(memPtr == metaData){
+            return true;
+        }
+        metaData = nextMetaData(metaData);
+    }
+    return false;
 }
 
 void *mymalloc(size_t size, char *file, int line){
