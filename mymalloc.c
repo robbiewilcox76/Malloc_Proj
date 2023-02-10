8f130126afbb580e3a2d399226b3142a12e0fa46
Sean M. Patrick <smp429@ilab2.cs.rutgers.edu>
2023-02-10 18:30:05 -0500

    Fixed malloc potentially

--- mymalloc.c
+++ mymalloc.c
@@ -10,12 +10,12 @@
 static char memory[MEMSIZE] = {INITFLAG};
 
 void insertMetaData(void *memPtr, short chunkSize){
-    short *metaData = (short *) memPtr;
+    short *metaData = memPtr;
     *metaData = chunkSize;
 }
 
 short getChunkSize(void *memPtr){
-    short *chunkSize = (short *) memPtr;
+    short *chunkSize = memPtr;
     //printf("%p", memPtr);
     return *chunkSize;
 }
@@ -33,33 +33,33 @@ void initializeMemory(){
         memset(memory, 0, MEMSIZE);
         char *metaDataPtr = memory;
         char *memEndPtr = memory + MEMSIZE;
-        short chunkSize = (memEndPtr - metaDataPtr - sizeof(short));
+        short chunkSize = (memEndPtr - metaDataPtr - 2);
        // printf("%d", chunkSize);
         insertMetaData(metaDataPtr, chunkSize);
     }
 }
 
-void *firstMetaData(){
+void *getFirstChunk(){
     char *memPtr = memory;
     return memPtr; //returns address of first memory chunk's metadata
 }
 
-void *nextMetaData(void *memPtr){ //returns address of next chunk's metadata given a starting chunk, NULL if there are no more chunks
-    short *nextChunkMetaData = memPtr;
-    short *memEnd = (short*)memory + MEMSIZE;
+void *getNextChunk(void *memPtr){ //returns address of next chunk's metadata given a starting chunk, NULL if there are no more chunks
+    char *nextChunk = memPtr;
+    char *memEnd = memory + MEMSIZE;
     short chunkSize = getChunkSize(memPtr);
     if(chunkSize < 0) {
         chunkSize *= -1;
     }
-    nextChunkMetaData += (chunkSize) + 1;
-    if(memEnd > nextChunkMetaData) {
-        return nextChunkMetaData;
+    nextChunk += chunkSize + 2;
+    if(memEnd > nextChunk) {
+        return nextChunk;
     }
     return NULL;
 }
 
 bool validPointer(void *memPtr){ //checks if a given pointer is within the bounds of the memory array
-    char *memStart = firstMetaData();
+    char *memStart = getFirstChunk();
     char *memEnd = memory + MEMSIZE;
     char *ptr = memPtr;
     if(ptr >= memStart && ptr < memEnd){
@@ -69,12 +69,12 @@ bool validPointer(void *memPtr){ //checks if a given pointer is within the bound
 }
 
 bool completePointer(void *memPtr){ //checks if a given pointer is complete(If it correctly points to the beginning of the memory chunk (its metadata) and not somewhere in between)
-    void *metaData = firstMetaData();
+    void *metaData = getFirstChunk();
     while(metaData != NULL){
         if(memPtr == metaData){
             return true;
         }
-        metaData = nextMetaData(metaData);
+        metaData = getNextChunk(metaData);
     }
     return false;
 }
@@ -83,7 +83,7 @@ void memError(char* file, int line, int error) {
     //0 = not enough space, 1 = double-free, 2 = bad pointer
     //printf("%s", file);
     switch(error) {
-        case(0): {printf("ERROR: Not enough available space"); return;}
+        case(0): {printf("ERROR: Not enough available space! Malloc called in %s on line %d ", file, line); return;}
         case(1): {printf("ERROR: Double free"); return;}
         case(2): {printf("ERROR: Pointer not allocated/bad pointer"); return;}
     }
@@ -91,28 +91,46 @@ void memError(char* file, int line, int error) {
 
 void *mymalloc(size_t size, char *file, int line){
     initializeMemory();
-    short* chunkFinder = firstMetaData();  //pointer to first chunk
-    while(!isChunkFree(chunkFinder) || *chunkFinder < size) {
-        chunkFinder = nextMetaData(chunkFinder);
-        if(chunkFinder == NULL) {/*need to throw error;*/ return NULL;} 
+    void* chunkFinder = getFirstChunk();  //pointer to first chunk
+    while(chunkFinder != NULL){
+        short chunkSize = getChunkSize(chunkFinder);
+        if(chunkSize < 0){
+            chunkSize *= -1;
+        }
+        if(isChunkFree(chunkFinder) && chunkSize >= size){
+            break;
+        }
+        chunkFinder = getNextChunk(chunkFinder);
     }
-    short oldblock = *chunkFinder;
-    *chunkFinder = (short)-size;
-    if(*(1+chunkFinder+(size)) == 0) {
-        *(1+chunkFinder+(size)) = (short)(oldblock-size-2);
+    if(chunkFinder == NULL){
+        memError(file, line, 0);
+        return NULL;
     }
+    short chunkSize = getChunkSize(chunkFinder);
+    if(chunkSize < 0){
+        chunkSize *= -1;
+    }
+    short remainingMemory = (chunkSize - (short)size - 2);
+    if(remainingMemory <= 0){
+        insertMetaData(chunkFinder, -(chunkSize));
+        return chunkFinder;
+    }
+    insertMetaData(chunkFinder, -(short)size);
+    insertMetaData((getNextChunk(chunkFinder)), remainingMemory);
     return chunkFinder;
 }
 
-void myfree(void *ptr, char *file, int line) {}
+void myfree(void *ptr, char *file, int line) {
+
+}
 
 int main(int argc, char **argv){
-    initializeMemory();
-    char* x = mymalloc(9, "hi", 3);
-    char* y = mymalloc(6, "hello", 4);
-    char* z = mymalloc(4, "hi", 3);
-    char* q = mymalloc(12, "hello", 4);
-    char* m = mymalloc(63, "hey," , 9);
+    char* x = malloc(9);
+    //strcpy(x, "abc");
+    char* y = malloc(6);
+    char* z = malloc(4);
+    char* q = malloc(12);
+    char* m = malloc(63);
 
     //prints array
     short* ptr = (short*)memory;
@@ -124,5 +142,16 @@ int main(int argc, char **argv){
     printf("\n%d",*z);
     printf("\n%d",*q);
     printf("\n%d",*m);
+
+    //   for(int i = 0; i < 4096; i ++){ //just checking if we can print every byte of the array
+    //       printf("|%d| ", memory[i]);
+    //   }
+
+    // short *p = (short *)getFirstChunk(); //checking if the size of allocated chunks are correct
+    // while(p != NULL){
+    //     printf("\n%d", *p);
+    //     p = getNextChunk(p);
+    // }
+
     return EXIT_SUCCESS;
 }
