209d3008408b1619fdc6920cbd3394268ead784c
Sean M. Patrick <smp429@ilab3.cs.rutgers.edu>
2023-02-12 17:09:12 -0500

    Finished memgrind but still need to figure out what is wrong

--- memgrind.c
+++ memgrind.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include "mymalloc.h"
 
 void task1(){
@@ -20,7 +21,7 @@ void task2(){
     }
 }
 
-void task3(){
+void task3(){ //randomly choose between allocating 1 byte or freeing 1 byte if possible, until 120 objects have been allocated
     char *pointers[120];
     int mallocCount = 0;
     int freeCount = 0;
@@ -40,48 +41,72 @@ void task3(){
    }
 }
 
-void task4(){
-    char* x;
-    char* y;
-    for(int i=0; i<400; i++) {
-        int num = (rand() % (2000)) + 10;
-        x = malloc(num);
-        y = malloc(num);
-        free(x);
-        free(y);
+void task4(){ //allocate all of memory in 30 byte chunks then free them all and request a larger chunk
+    char *pointers[128];
+    for(int i = 0; i < 128; i ++){
+        pointers[i] = malloc(30);
+    }
+    for(int i = 0; i < 128; i++){
+        free(pointers[i]);
+    }
+    char *x = malloc(60);
+    free(x);
+}
+
+void task5(){ //randomly choose between allocating variable size chunk or freeing chunk if possible until 120 objects have been allocated
+    char *pointers[120];
+    int mallocCount = 0;
+    int freeCount = 0;
+    while(mallocCount != 120){
+        int x = rand() % 2;
+        if(x == 0){
+            pointers[mallocCount++] = malloc((rand() % 32) + 1);
+        }
+        else{
+            if(freeCount != mallocCount){
+                free(pointers[freeCount++]);
+            }
+        }
+    }
+    for(int i = freeCount; i != mallocCount; i ++){
+        free(pointers[i]);
     }
 }
 
-void task5(){ //need to design our own test
-
-}
-
 int main(int argc, char **argv){
-    task1();
-    task2();
-    task3();
-    task4();
-    //char* x = malloc(11);
-    //char* y = malloc(11);
-    //free(y);
-    //char* z = malloc(10);
-    //free(x);
-    //free(z);
-    /*char *x = malloc(7);
-    //printMemory();
-    strcpy(x, "helloo");
-    printf("%s\n", x);
-    free(x);
-    char *y = malloc(2);
-    strcpy(y, "h");
-    printf("%s\n", y);
-    //printMemory();
-    free(y);
-    //printMemory();
-    // printMemory();
-    char *z = malloc(2); 
-    strcpy(z, "z");
-    printf("%s", z);
-    free(z);*/
+    double totalTimes[5] = {0, 0, 0, 0, 0};
+    double currentTimes[5] = {0, 0, 0, 0, 0};
+    struct timeval start, end;
+    for(int i = 0; i < 50; i ++){
+        printf("Iteration %d:\n", i+1);
+        gettimeofday(&start, NULL);
+        task1();
+        gettimeofday(&end, NULL);
+        currentTimes[0] = ((end.tv_sec * 1000000 + end.tv_usec * 1000000) - (start.tv_sec * 1000000 + start.tv_usec * 1000000));
+        gettimeofday(&start, NULL);
+        task2();
+        gettimeofday(&end, NULL);
+        currentTimes[1] = ((end.tv_sec * 1000000 + end.tv_usec * 1000000) - (start.tv_sec * 1000000 + start.tv_usec * 1000000));
+        gettimeofday(&start, NULL);
+        task3();
+        gettimeofday(&end, NULL);
+        currentTimes[2] = ((end.tv_sec * 1000000 + end.tv_usec * 1000000) - (start.tv_sec * 1000000 + start.tv_usec * 1000000));
+        gettimeofday(&start, NULL);
+        task4();
+        gettimeofday(&end, NULL);
+        currentTimes[3] = ((end.tv_sec * 1000000 + end.tv_usec * 1000000) - (start.tv_sec * 1000000 + start.tv_usec * 1000000));
+        gettimeofday(&start, NULL);
+        task5();
+        gettimeofday(&end, NULL);
+        currentTimes[4] = ((end.tv_sec * 1000000 + end.tv_usec * 1000000) - (start.tv_sec * 1000000 + start.tv_usec * 1000000));
+        for(int j = 0; j < 5; j ++){
+            totalTimes[j] += currentTimes[j];
+            printf("Time spent on task %d: %lf microseconds\n", j+1, currentTimes[j]);
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < 5; i ++){
+        printf("Average time spent on task %d: %lf microseconds\n", i+1, (totalTimes[i]/50));
+    }
     return EXIT_SUCCESS;
 }
--- test.c
+++ test.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mymalloc.h"
 
-int main() {
-    printf("Testing github on iLab ");
+int main(int argc, char **argv) {
+    /*
+    3. Write a program that allocates several large objects. Once allocation is complete, it fills each
+    object with a distinct byte pattern (e.g., the first object is filled with 1, the second with 2,
+    etc.). Finally, it checks whether each object still contains the written pattern. (That is, writing
+    to one object did not overwrite any other.)
+    */
+    return EXIT_SUCCESS;
 }
